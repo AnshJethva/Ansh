4cae74ff85940b9b0de38ef59347393ff266e44f
AnshJethva <anshjethva1801@outlook.com>
2026-01-24 05:39:09 +0000

    4

--- Day-2/4.py
+++ Day-2/4.py
@@ -1,1 +1,19 @@
 
+<<<<<<< HEAD
+=======
+nums = [1,2,3]
+
+cart = ["phone", "book", 10, False, None, {1: "a"}, nums]
+
+print(cart)
+
+cart[0] = "pc"
+
+print(cart)
+print(type(cart))
+
+marks = (55,66,77,88,99)
+print(marks)
+
+print(type(marks))
+>>>>>>> 9d1242f (4)
